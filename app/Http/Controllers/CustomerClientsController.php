cf5344a0c4838c8383bdc501de8d2b5d3320f098
xeon <jjeeraldo@gmail.com>
2019-10-08 16:58:08 +0800

    Portal Updates

--- app/Http/Controllers/CustomerClientsController.php
+++ app/Http/Controllers/CustomerClientsController.php
@@ -51,35 +51,45 @@ class CustomerClientsController extends Controller
     }
 
     public function index(){
-        $url        = 'https://localhost:44329/api/UserWallet';
-        //$cookieFile = tempnam(null, 'session');
-        $cookieFile = $_SESSION['session_cookie'];
-        $userAgent  = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:11.0) Gecko/20100101 Firefox/11.0';
 
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_COOKIEFILE, $cookieFile);
-        //curl_setopt($ch, CURLOPT_COOKIEJAR,  $cookieFile);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_USERAGENT,  $userAgent); // empty user agents probably not accepted
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($ch, CURLOPT_AUTOREFERER,    1); // enable this - they check referer on POST
-        //curl_setopt($ch, CURLOPT_POST, 1);
-        //curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-        //curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-        //    'Content-Type: application/json',
-        //    'Content-Length: ' . strlen($post))
-        //);
+        if (isset($_SESSION['session_cookie']))
+        {
+            $url        = 'https://localhost:44329/api/UserWallet';
+            //$cookieFile = tempnam(null, 'session');
+            $cookieFile = $_SESSION['session_cookie'];
+            $userAgent  = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:11.0) Gecko/20100101 Firefox/11.0';
 
-        $html = curl_exec($ch);
-        $_res = json_decode($html);
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_COOKIEFILE, $cookieFile);
+            //curl_setopt($ch, CURLOPT_COOKIEJAR,  $cookieFile);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_USERAGENT,  $userAgent); // empty user agents probably not accepted
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            curl_setopt($ch, CURLOPT_AUTOREFERER,    1); // enable this - they check referer on POST
+            //curl_setopt($ch, CURLOPT_POST, 1);
+            //curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+            //curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            //    'Content-Type: application/json',
+            //    'Content-Length: ' . strlen($post))
+            //);
 
-        if ($_res->httpStatusCode === '500') {
-            //return redirect('sign-in');
-            session_destroy();
-            return Redirect::action('PagesController@signin')->with('failAttempt', $_res->message);
+            $html = curl_exec($ch);
+            $_res = json_decode($html);
+
+            if ($_res->httpStatusCode === '500') {
+                //return redirect('sign-in');
+                session_destroy();
+                return Redirect::action('PagesController@signin')->with('failAttempt', $_res->message);
+            }
+            else{return view('frontend.customer-clients.index', ["user_data"=>$_res] );}
         }
-        else{return view('frontend.customer-clients.index', ["user_data"=>$_res] );}
+        else{
+            session_destroy();
+            return Redirect::action('PagesController@signin');
+        }
+
+
     }
 
     public function walletBitcoin()
--- resources/views/frontend/customer-clients/index.blade.php
+++ resources/views/frontend/customer-clients/index.blade.php
@@ -183,7 +183,55 @@
 							<strong >Transaction History</strong>		
 					   </div>
 					   
-					   <div class = "panel-body">
+					    <div class="panel-body">
+					<div class="transactions__filters">
+
+						<div class="transactions__filters-col" style="margin-left: -30px;">
+							
+							<div class="custom-select--grey custom-select--actions">
+								<select class="custom-select">
+									<option value="All transactions">All transactions</option>
+									<option value="Sent">Sent</option>
+									<option value="Pending">Pending</option>
+									<option value="Recieved">Recieved</option>
+									<option value="Decline">Decline</option>
+								</select>
+							</div>
+							
+						</div>
+
+						<div class="transactions__filters-col transactions__filters-col--align-right">
+							<div class="datapickers">
+								<div class="datapicker-range">
+									<img src="img/calendar.svg" alt="">
+									<input type="text" placeholder="2017-04-01" class="datepicker-input datepicker-here" data-date-format="yyyy-mm-dd">
+								</div>
+								<div class="separator"></div>
+								<div class="datapicker-range">
+									<img src="img/calendar.svg" alt="">
+									<input type="text" placeholder="2017-04-01" class="datepicker-input datepicker-here" data-date-format="yyyy-mm-dd">
+								</div>
+							</div>
+							
+							</div>
+
+
+							<div class="custom-select--grey custom-select--currency">
+								<select class="custom-select">
+									<option value="Currency">Currency</option>
+									<option value="Ethereum">Ethereum</option>
+									<option value="Litecoin">Bitcoincash</option>
+								</select>
+							</div>
+
+						
+							<div class="input-wrap search-input">
+								<img src="img/search.svg" alt="">
+								<input type="text" placeholder="Search">
+							</div>
+						</div>
+			
+
 					   </div>
 					</div>
 				</div>
